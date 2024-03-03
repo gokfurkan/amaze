6788a56a032e467381110c62ddcd7222986f0259
Furkan Gök <furkangok270@gmail.com>
2024-03-04 00:43:46 +0300

    add camera controller

---
+++ Assets/Game/Dev/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Game.Dev.Scripts.Scriptables;
+using Template.Scripts;
+using UnityEngine;
+
+namespace Game.Dev.Scripts
+{
+    public class CameraController : MonoBehaviour
+    {
+        private LevelOptions levelOptions;
+
+        private void Awake()
+        {
+            levelOptions = InfrastructureManager.instance.gameSettings.levelOptions;
+        }
+
+        private void Start()
+        {
+            InitCamera();
+        }
+
+        private void InitCamera()
+        {
+            int currentLevel = LevelSpawnManager.instance.levelIndex;
+            
+            transform.position = levelOptions.levelDataOptions[currentLevel].cameraPos;
+            transform.eulerAngles = levelOptions.levelDataOptions[currentLevel].cameraRot;
+        }
+    }
+}
