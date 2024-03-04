569ec41c055def51dde05eac3f5439974998ab2c
Furkan Gök <furkangok270@gmail.com>
2024-03-05 00:56:01 +0300

    add game icon , get ui color system , update panels , dev panel system

---
+++ Assets/Game/Dev/Scripts/GetUIColor.cs
@@ -0,0 +1,35 @@
+﻿using Template.Scripts;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.Dev.Scripts
+{
+    public class GetUIColor : MonoBehaviour
+    {
+        private void Awake()
+        {
+            InitUIColor();
+        }
+
+        private void InitUIColor()
+        {
+            Color color;
+            
+            switch (SaveManager.instance.saveData.environmentIndex)
+            {
+                case 0:
+                case 1:
+                    color = Color.black;
+                    break;
+                case 2:
+                    color = Color.white;
+                    break;
+                default:
+                    color = Color.clear;
+                    break;
+            }
+            
+            GetComponent<Graphic>().color = color;
+        }
+    }
+}
--- Assets/Template/Scripts/DevPanelController.cs
+++ Assets/Template/Scripts/DevPanelController.cs
@@ -6,11 +6,20 @@ namespace Template.Scripts
 {
     public class DevPanelController : MonoBehaviour
     {
-        public InputField ipf;
+        public InputField level;
+        public InputField environment;
 
         public void ChangeLevel()
         {
-            SaveManager.instance.saveData.level = int.Parse(ipf.text);
+            SaveManager.instance.saveData.level = int.Parse(level.text);
+            SaveManager.instance.Save();
+            
+            BusSystem.CallLevelLoad();
+        }
+        
+        public void ChangeEnvironment()
+        {
+            SaveManager.instance.saveData.environmentIndex = int.Parse(environment.text);
             SaveManager.instance.Save();
             
             BusSystem.CallLevelLoad();
